e6efe419a1a6bad8f49f3b3ecba493560254fc51
chre55551 <chre55551@gmail.com>
2021-01-12 15:11:17 +0800

    0112
    1511

--- src/main/java/hotel/yun/ordered/model/Ordered.java
+++ src/main/java/hotel/yun/ordered/model/Ordered.java
@@ -21,6 +21,8 @@ public class Ordered implements Serializable{
 	private int ordered_number;// 訂單編號
 	
 	private int customer_id;// 會員編號，必須建其他資料表才吃的到
+	private int ordered_tomeals_id;//餐點訂單編號
+	private int ordered_toroom_id;//房間訂單編號
 	private int status_id;// 狀態ID，必須建其他資料表才吃的到
 	private int payment_id;// 付款ID，必須建其他資料表才吃的到
 	private int ordered_accounts;// 訂單總價
@@ -44,88 +46,6 @@ public class Ordered implements Serializable{
 
 	}
 
-	public int getOrdered_number() {
-		return ordered_number;
-	}
 
-	public void setOrdered_number(int ordered_number) {
-		this.ordered_number = ordered_number;
-	}
-
-	public int getCustomer_id() {
-		return customer_id;
-	}
-
-	public void setCustomer_id(int customer_id) {
-		this.customer_id = customer_id;
-	}
-
-	public int getStatus_id() {
-		return status_id;
-	}
-
-	public void setStatus_id(int status_id) {
-		this.status_id = status_id;
-	}
-
-	public int getPayment_id() {
-		return payment_id;
-	}
-
-	public void setPayment_id(int payment_id) {
-		this.payment_id = payment_id;
-	}
-
-	public int getOrdered_accounts() {
-		return ordered_accounts;
-	}
-
-	public void setOrdered_accounts(int ordered_accounts) {
-		this.ordered_accounts = ordered_accounts;
-	}
-
-	public Date getOrdered_date() {
-		return ordered_date;
-	}
-
-	public void setOrdered_date(Date ordered_date) {
-		this.ordered_date = ordered_date;
-	}
-
-	public int getIv_no() {
-		return iv_no;
-	}
-
-	public void setIv_no(int iv_no) {
-		this.iv_no = iv_no;
-	}
-
-	public String getNote() {
-		return note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
-	}
-
-	public OrderedStatus getOrderedStatus() {
-		return orderedStatus;
-	}
-
-	public void setOrderedStatus(OrderedStatus orderedStatus) {
-		this.orderedStatus = orderedStatus;
-	}
-
-	public OrderedPayment getOrderedPayment() {
-		return orderedPayment;
-	}
-
-	public void setOrderedPayment(OrderedPayment orderedPayment) {
-		this.orderedPayment = orderedPayment;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 
 }
---
+++ src/main/java/hotel/yun/ordered/model/OrderedToRoom.java
@@ -0,0 +1,89 @@
+package hotel.yun.ordered.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordered_toroom")
+public class OrderedToRoom {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int ordered_toroom_id;
+	
+	private int room_id;
+	private int room_accounts;
+	private int number_of_room;
+	private Date room_ordered_time;
+	private int room_number;
+	
+	//private Room room;
+	
+	public OrderedToRoom() {
+		
+	}
+	
+	public OrderedToRoom(int ordered_toroom_id, int room_id, int room_accounts, int number_of_room,
+			Date room_ordered_time, int room_number) {
+		this.ordered_toroom_id = ordered_toroom_id;
+		this.room_id = room_id;
+		this.room_accounts = room_accounts;
+		this.number_of_room = number_of_room;
+		this.room_ordered_time = room_ordered_time;
+		this.room_number = room_number;
+	}
+
+	public int getOrdered_toroom_id() {
+		return ordered_toroom_id;
+	}
+
+	public void setOrdered_toroom_id(int ordered_toroom_id) {
+		this.ordered_toroom_id = ordered_toroom_id;
+	}
+
+	public int getRoom_id() {
+		return room_id;
+	}
+
+	public void setRoom_id(int room_id) {
+		this.room_id = room_id;
+	}
+
+	public int getRoom_accounts() {
+		return room_accounts;
+	}
+
+	public void setRoom_accounts(int room_accounts) {
+		this.room_accounts = room_accounts;
+	}
+
+	public int getNumber_of_room() {
+		return number_of_room;
+	}
+
+	public void setNumber_of_room(int number_of_room) {
+		this.number_of_room = number_of_room;
+	}
+
+	public Date getRoom_ordered_time() {
+		return room_ordered_time;
+	}
+
+	public void setRoom_ordered_time(Date room_ordered_time) {
+		this.room_ordered_time = room_ordered_time;
+	}
+
+	public int getRoom_number() {
+		return room_number;
+	}
+
+	public void setRoom_number(int room_number) {
+		this.room_number = room_number;
+	}
+	
+}
