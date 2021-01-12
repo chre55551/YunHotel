fe8cb4bf36020b3c6f46086d0c4d5417f37fe66d
koncan <m820730@gmail.com>
2021-01-12 18:11:40 +0800

    0112
    1811

--- src/main/java/hotel/yun/employee/dao/Employee_Dao.java
+++ src/main/java/hotel/yun/employee/dao/Employee_Dao.java
@@ -7,9 +7,9 @@ import hotel.yun.employee.model.Employee_basic;
 public interface Employee_Dao {
 	
 	public Employee_basic insert(Employee_basic emBean);
-	public Employee_basic query(int employee_id);
+	public boolean query(Integer employee_id);
 	public List<Employee_basic> queryAll();
 	public Employee_basic update(Employee_basic embean);
-	public boolean delete(int employee_id);
+	public void delete(Integer employee_id) ;
 	
 }
--- src/main/java/hotel/yun/employee/dao/impl/Employee_DaoImpl.java
+++ src/main/java/hotel/yun/employee/dao/impl/Employee_DaoImpl.java
@@ -3,6 +3,9 @@ package hotel.yun.employee.dao.impl;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.NoResultException;
+
+import org.hibernate.NonUniqueResultException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import hotel.yun.employee.dao.Employee_Dao;
 import hotel.yun.employee.model.Employee_basic;
+
 
 @Repository
 public class Employee_DaoImpl implements Serializable, Employee_Dao{
@@ -26,26 +30,70 @@ public class Employee_DaoImpl implements Serializable, Employee_Dao{
 	}
 	
 	@Override
-	public Employee_basic query(int employee_id) {
-		
-		return null;
+	public boolean query(Integer employee_id) {
+		boolean exist = false;
+		String hql = "FROM Employee_basic WHERE employee_id = :employee_id";
+		Session session = factory.getCurrentSession();
+		Employee_basic employee_basic = null;
+		try {
+			employee_basic = (Employee_basic) session.createQuery(hql)
+					.setParameter("employee_id", employee_id)
+					.getSingleResult();
+		} catch(NoResultException | NonUniqueResultException e){
+			;
+		}
+		if (employee_basic != null) { 
+			exist = true;
+		}
+	return exist;
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<Employee_basic> queryAll() {
-		
-		return null;
-	}
+		// 查詢所有紀錄
+
+			String hql = "FROM Employee_basic"; 
+			List<Employee_basic> allEmployees = null;
+			Session session = factory.getCurrentSession();
+			allEmployees = session.createQuery(hql).getResultList();
+			return allEmployees;
+		}
 
 	@Override
 	public Employee_basic update(Employee_basic emBean) {
+		Session session = factory.getCurrentSession();
+		session.update(emBean);
+		return emBean;
 		
-		return null;
 	}
+	
 
+	// 刪除紀錄
 	@Override
-	public boolean delete(int employee_id) {
-		
-		return false;
+	public void delete(Integer employee_id) {
+		Session session = factory.getCurrentSession();
+		Employee_basic emBean = new Employee_basic();
+		emBean.setEmployee_id(employee_id);
+		session.delete(emBean);
 	}
+//	public void delete(int employee_id,String employee_name,String employee_department,String employee_position)  {
+//		Session session = factory.getCurrentSession();
+//		Transaction tx =null;
+//		try {
+//			Employee_basic emBean = new Employee_basic(employee_id,employee_name,employee_department,employee_position);
+//			session.delete(emBean);   
+//		} catch(Exception e){
+//			String classname5 =  e.getCause().getClass().getName();
+//			String classname4 =  e.getClass().getName();
+//			if ( classname5.equalsIgnoreCase("org.hibernate.StaleStateException") || 
+//				classname4.equalsIgnoreCase("org.hibernate.StaleStateException")	
+//			) {
+//				throw new RecordNotFoundException("要刪除的紀錄不存在: 主鍵值為: " + employee_id);
+//			} else {
+//				throw new RecordNotFoundException("刪除紀錄時發生異常", e);
+//			}
+//		}
+//		return;
+//	}
 }
