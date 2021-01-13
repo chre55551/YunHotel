6a4b961ad7a078a4523c880d3988444073af5b1e
chre55551 <chre55551@gmail.com>
2021-01-13 20:09:38 +0800

    0113
    2009

--- src/main/java/hotel/yun/ordered/controller/Ordered_Controller.java
+++ src/main/java/hotel/yun/ordered/controller/Ordered_Controller.java
@@ -8,12 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import hotel.yun.ordered.model.Ordered;
 import hotel.yun.ordered.service.Ordered_Service;
 
 @Controller
 @RequestMapping("/hotel.yun.ordered")
+//我的這個跨請求是只要設外鍵需要連結到另一個請求頁面，就要把它設進去沒錯吧?
+@SessionAttributes({"order_number", "customer_id","ordered_tomeals_id","ordered_toroom_id","status_id","payment_id"})
 public class Ordered_Controller {
 	
 	@Autowired
@@ -32,9 +35,7 @@ public class Ordered_Controller {
 	
 	@PostMapping("")
 	public String insert(@ModelAttribute Ordered bean) {
-		
-		
-		
+	
 		return null;
 		
 	}
