3578f562b4cd0ba6023bbc8107d625c7a8c57c86
poky172839456 <75819523+poky172839456@users.noreply.github.com>
2021-01-13 11:43:20 +0800

    0113
    11:43

--- src/main/java/hotel/yun/customer/service/CustomerService.java
+++ src/main/java/hotel/yun/customer/service/CustomerService.java
@@ -1,5 +1,19 @@
 package hotel.yun.customer.service;
 
-public class CustomerService {
+import java.util.List;
 
+
+import hotel.yun.customer.model.Customer;
+
+public interface CustomerService {
+	Customer getCustomerById(int id);
+
+	List<Customer> getCustomers();
+
+	Object save(Customer bean);
+	
+	void updateCustomer(Customer bean); 
+
+	void deleteCustomerByPrimaryKey(int key);
+	
 }
