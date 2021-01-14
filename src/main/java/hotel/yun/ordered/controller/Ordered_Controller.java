57e3dcc45c2e89a15cf878e0394db706905af5ff
chre55551 <chre55551@gmail.com>
2021-01-14 18:49:59 +0800

    0114
    1849

--- src/main/java/hotel/yun/ordered/controller/Ordered_Controller.java
+++ src/main/java/hotel/yun/ordered/controller/Ordered_Controller.java
@@ -5,8 +5,10 @@ import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -16,41 +18,34 @@ import hotel.yun.ordered.service.Ordered_Service;
 
 @Controller
 @RequestMapping("/hotel.yun.ordered")
-//我的這個跨請求是只要設外鍵需要連結到另一個請求頁面，就要把它設進去沒錯吧?
-@SessionAttributes({"order_number", "customer_id","ordered_tomeals_id","ordered_toroom_id","status_id","payment_id"})
+@SessionAttributes({ "order_number", "customer_id", "ordered_tomeals_id", "ordered_toroom_id", "status_id",
+		"payment_id" })
 public class Ordered_Controller {
-	
+
 	@Autowired
 	ServletContext context;
-	
-	public void setContext(ServletContext context) {
-		this.context = context;
-	}
-	
+
 	@Autowired
 	Ordered_Service service;
-
-	public void setService(Ordered_Service service) {
-		this.service = service;
-	}
 
 	// 本方法於新增時，送出空白的表單讓使用者輸入資料
 	@GetMapping("/ShowOrdered")
 	public String ShowOrdered(Model model) {
 		Ordered od = new Ordered();
 		model.addAttribute("ordered", od);
-		String s = "ordered/InsertOrdered";
-		return s;
+		return "ordered/InsertOrdered";
 	}
-	//給他輸入
+	// 讓使用者輸入
 	@PostMapping("/ShowOrdered")
-	public String insert(@ModelAttribute("od") Ordered od,Model model) {
+	public String insert(@ModelAttribute("od") Ordered od, Model model) {
 		service.insert(od);
 		return "redirect";
 	}
-	
-	public String query() {
-		return null;
-		
+
+	// 從訂單編號刪除一筆訂單
+	@DeleteMapping("/ShowOrdered/{ordered_number}")
+	public String delete(@PathVariable("ordered_number") int ordered_number) {
+		service.delete(ordered_number);
+		return "redirect:/ShowOrdered";
 	}
 }
